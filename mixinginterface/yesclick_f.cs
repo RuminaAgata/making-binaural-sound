b8093a06afce8daff4041b0cc19d544d7e308e75
RuminaAgata <60873500+RuminaAgata@users.noreply.github.com>
2020-02-11 01:59:26 +0900

    Create yesclick_f.cs

---
+++ mixinginterface/yesclick_f.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using UnityEngine.UI;
+public class yesclick_f : MonoBehaviour, clickbutton.Iyesclick_f
+{
+    public allrecord all;
+    public float time = 16f;
+    public void OnEyeControllerHit(bool isOn)
+    {
+        // 視線マーカーがヒットしたら色を変える
+        gameObject.GetComponent<Renderer>().material.color = isOn ? new Color(1, 1, 0) : Color.white;
+    }
+
+    public void OnEyeControllerClick()
+    {
+        Debug.Log("yes");
+        all.yesfclick();       
+      //  all.onrecord();
+ 
+
+    }
+}
