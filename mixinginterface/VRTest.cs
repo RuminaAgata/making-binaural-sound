6384c5f4370a11cad5bed4a6b870cbe01d334d42
RuminaAgata <60873500+RuminaAgata@users.noreply.github.com>
2020-02-11 01:59:23 +0900

    Create VRTest.cs

---
+++ mixinginterface/VRTest.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Valve.VR;
+
+public class VRTest : MonoBehaviour
+{
+    /*private SteamVR_Action_Boolean actionToHaptic = SteamVR_Actions._default.Menu;
+    private SteamVR_Action_Vibration haptic = SteamVR_Actions._default.Haptic;
+
+    private void Update()
+    {
+        if (actionToHaptic.GetStateDown(SteamVR_Input_Sources.LeftHand))
+        {
+            haptic.Execute(0, 0.1f, 60, 1, SteamVR_Input_Sources.LeftHand);
+        }
+    }*/
+}
