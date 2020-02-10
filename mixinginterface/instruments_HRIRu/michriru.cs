94924507a036460a37c04578947908bf3fc91c6b
RuminaAgata <60873500+RuminaAgata@users.noreply.github.com>
2020-02-11 01:59:42 +0900

    Create michriru.cs

---
+++ mixinginterface/instruments_HRIRu/michriru.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class michriru : MonoBehaviour,allrecord.Imicrestart
+{
+    private Vector3 velocity;
+    private HRIRu hrir_control;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        hrir_control = this.GetComponent<HRIRu>();
+        hrir_control.Available();
+        hrir_control.Play("ShyVoice.wav");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void restart()
+    {
+        hrir_control.Play("ShyVoice.wav");
+    }
+}
