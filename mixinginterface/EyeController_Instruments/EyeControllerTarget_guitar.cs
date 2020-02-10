0966e308eb4aba32a2e638f2543bc6514c2eb51d
RuminaAgata <60873500+RuminaAgata@users.noreply.github.com>
2020-02-11 02:02:37 +0900

    Create EyeControllerTarget_guitar.cs

---
+++ mixinginterface/EyeController_Instruments/EyeControllerTarget_guitar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class EyeControllerTarget_guitar : MonoBehaviour, EyeController.IEyeControllerTarget_guitar, clickbutton.IEyeControllerTarget_guitar
+{
+    public static bool guitarrecorded;
+    public void OnEyeControllerHit(bool isOn)
+    {
+        Debug.Log("hit");
+        // 視線マーカーがヒットしたら色を変える
+        gameObject.GetComponent<Renderer>().material.color = isOn ? new Color(1, 1, 0) : Color.white;
+    }
+
+    public void OnEyeControllerClick()
+    {
+        Debug.Log("scene");
+        // 視線マーカーでクリックしたらシーンを変える
+        SceneManager.LoadScene("guitarrec");
+    }
+    public void OnEyeControllerClick_f()
+    {
+        guitarrecorded = TestRecorder.getguitarrec();
+        guitarrecorded = false;
+        SceneManager.LoadScene("guitarrec");
+    }
+}
