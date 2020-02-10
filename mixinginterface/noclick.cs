7b10765d2033ede5df1eeb6108fcea0642a7a08c
RuminaAgata <60873500+RuminaAgata@users.noreply.github.com>
2020-02-11 01:59:34 +0900

    Create noclick.cs

---
+++ mixinginterface/noclick.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Valve.VR.Extras;
+using UnityEngine.SceneManagement;
+
+public class noclick : MonoBehaviour , clickbutton.Inoclick
+{
+    public TestRecorder ob;
+    public void OnEyeControllerHit(bool isOn)
+    {
+        // 視線マーカーがヒットしたら色を変える
+        gameObject.GetComponent<Renderer>().material.color = isOn ? new Color(1, 1, 0) : Color.white;
+    }
+
+    public void OnEyeControllerClick()
+    {
+        ob.NotRestart();
+        // 視線マーカーでクリックしたら SceneManager.LoadScene("scene1");シーンを変える
+       
+    }
+}
