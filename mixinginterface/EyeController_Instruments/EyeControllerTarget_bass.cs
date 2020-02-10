7a07909f64093cf1445467a91e253f127afcc41a
RuminaAgata <60873500+RuminaAgata@users.noreply.github.com>
2020-02-11 02:02:40 +0900

    Create EyeControllerTarget_bass.cs

---
+++ mixinginterface/EyeController_Instruments/EyeControllerTarget_bass.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class EyeControllerTarget_bass : MonoBehaviour, EyeController.IEyeControllerTarget_bass,clickbutton.IEyeControllerTarget_bass
+{
+    public static bool bassrecorded;
+    public void OnEyeControllerHit(bool isOn)
+    {
+        Debug.Log("hit");
+        // 視線マーカーがヒットしたら色を変える
+        gameObject.GetComponent<Renderer>().material.color = isOn ? new Color(1, 1, 0) : Color.white;
+    }
+
+    public void OnEyeControllerClick()
+    {
+        Debug.Log("scene");
+        // 視線マーカーでクリックしたらシーンを変える
+        SceneManager.LoadScene("bassrec");
+    }
+    public void OnEyeControllerClick_f()
+    {
+        bassrecorded = TestRecorder.getpianorec();
+        bassrecorded=false;
+        SceneManager.LoadScene("bassrec");
+    }
+}
